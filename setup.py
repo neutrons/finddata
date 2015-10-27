3a07ba2223506284b98ab0bfbe355defd681a6dc
Pete Peterson <petersonpf@ornl.gov>
2015-10-26 23:14:31 -0400

    Filling in more metadata in setup

--- setup.py
+++ setup.py
@@ -2,4 +2,10 @@ from distutils.core import setup
 
 setup(name="finddata",
       version="0.2.2",
+      description = "Find data files using ICAT",
+      author = "Pete Peterson",
+      #author_email = "email@someplace.com",
+      url = "http://github.com/peterfpeterson/finddata/"
+      long_description = """This package uses ICAT4 at SNS to find NeXus files.""",
+      license = "The MIT License (MIT)",
       scripts=["finddata"])

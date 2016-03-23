06ecf27e1786acbdf77aec88ac6ea0cd8fd5d6b9
Pete Peterson <petersonpf@ornl.gov>
2016-03-23 11:02:33 -0400

    Setting up versioneer

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
+import versioneer # https://github.com/warner/python-versioneer
 from distutils.core import setup
 
 setup(name="finddata",
-      version="0.2.2",
+      version=versioneer.get_version(), #"0.2.2",
+      cmdclass=versioneer.get_cmdclass(),
       description = "Find data files using ICAT",
       author = "Pete Peterson",
       author_email = "petersonpf@ornl.gov",
       url = "http://github.com/peterfpeterson/finddata/",
       long_description = """This package uses ICAT4 at SNS to find NeXus files.""",
       license = "The MIT License (MIT)",
-      scripts=["finddata"],
+      scripts=["scripts/finddata"],
+      packages=["finddata"],
+      package_dir={},#'finddata': '.'},
       data_files=[('/etc/bash_completion.d/', ['finddata.bashcomplete'])]
 )

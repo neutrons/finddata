5bc640b7853a4d036c6e49fd44b2fecf2469c51b
Pete Peterson <petersonpf@ornl.gov>
2023-11-08 15:03:49 -0500

    Drop python2 support and add rpmbuild.sh (#13)
    * Remove future imports
    
    * Drop python2 package
    
    * Add script for building the rpm
    
    * Abstract away the distribution number from the final message
    
    * Change to urllib3

---
+++ rpmbuild.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# get the version from the spec file
+SPECFILE="$(dirname "$(realpath "$0")")/finddata.spec"
+echo "Finding version from ${SPECFILE}"
+
+VERSION="$(grep Version "${SPECFILE}" | awk '{print $2}')"
+if [ -z "${VERSION}" ]; then
+    echo "Failed to determine the version from ${SPECFILE}"
+    exit 127
+fi
+echo "Version is ${VERSION}"
+
+# create the tarball
+TARBALL="finddata-v${VERSION}.tar.gz"
+echo "creating tarball ${TARBALL}"
+git archive --format=tar.gz HEAD --prefix="finddata-${VERSION}/" -o "${TARBALL}"
+mkdir -p "${HOME}"/rpmbuild/SOURCES
+cp "${TARBALL}" "${HOME}/rpmbuild/SOURCES/v${VERSION}.tar.gz"
+
+# build the rpm and give instructions
+echo "building the rpm"
+rpmbuild -ba finddata.spec || exit 127
+
+DIST=$(rpm --eval %{?dist})
+echo "========================================"
+echo "Successfully built rpm. To manually inspect package run"
+echo "rpm -qilRp ~/rpmbuild/RPMS/noarch/python3-finddata-${VERSION}-1${DIST}.noarch.rpm"

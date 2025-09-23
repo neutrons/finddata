63755b63a7ce7cb1b71dd84417cc0e1d8a229585
Darsh <ddarsh5544@gmail.com>
2025-09-23 12:02:21 -0400

    Remove publish_plot from findata

--- src/finddata/__init__.py
+++ src/finddata/__init__.py
@@ -4,4 +4,11 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from .publish_plot import publish_plot  # noqa: F401
+# Import publish_plot functions from external package if available
+# This maintains backward compatibility while removing local duplicate code
+try:
+    from plot_publisher import plot1d, plot_heatmap, publish_plot  # noqa: F401
+except ImportError:
+    # plot_publisher not available - functions will not be accessible
+    # but module will still import successfully
+    pass

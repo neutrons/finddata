156ecaebde8e02255995e1cab1e37fcda72a8ba1
Darsh Dinger <106342815+darshdinger@users.noreply.github.com>
2026-07-24 14:27:40 -0400

    Remove plot_publisher from finddata (#88)

--- src/finddata/__init__.py
+++ src/finddata/__init__.py
@@ -3,12 +3,3 @@ from importlib import metadata
 
 __version__ = metadata.version("finddata")
 del metadata
-
-# Import publish_plot functions from external package if available
-# This maintains backward compatibility while removing local duplicate code
-try:
-    from plot_publisher import plot1d, plot_heatmap, publish_plot  # noqa: F401
-except ImportError:
-    # plot_publisher not available - functions will not be accessible
-    # but module will still import successfully
-    pass
